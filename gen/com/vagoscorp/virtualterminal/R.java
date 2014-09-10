a308ab683fe3b6fcf835cb21c1df268cdc16f499
Sebasu <Sebasu_Knutti@hotmail.com>
2014-09-09 22:25:38 -0400

    Res Update

--- gen/com/vagoscorp/virtualterminal/R.java
+++ gen/com/vagoscorp/virtualterminal/R.java
@@ -9,18 +9,18 @@ package com.vagoscorp.virtualterminal;
 
 public final class R {
     public static final class array {
-        public static final int Cases=0x7f070000;
-        public static final int fin=0x7f070001;
+        public static final int Cases=0x7f060000;
+        public static final int fin=0x7f060001;
     }
     public static final class attr {
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
          */
-        public static final int activity_horizontal_margin=0x7f060000;
-        public static final int activity_vertical_margin=0x7f060001;
-        public static final int px20=0x7f060003;
-        public static final int px30=0x7f060002;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+        public static final int px20=0x7f040003;
+        public static final int px30=0x7f040002;
     }
     public static final class drawable {
         public static final int ic_bt=0x7f020000;
@@ -69,55 +69,55 @@ public final class R {
         public static final int rbdojqw=0x7f080003;
     }
     public static final class string {
-        public static final int BRX=0x7f04000e;
-        public static final int BSend=0x7f040005;
-        public static final int BSendn=0x7f040006;
-        public static final int BTX=0x7f04000f;
-        public static final int BT_Device=0x7f040022;
-        public static final int Button_Conect=0x7f040003;
-        public static final int Button_Des=0x7f04001f;
-        public static final int Button_Sel=0x7f040018;
-        public static final int Cancel_Conn=0x7f040023;
-        public static final int Chan_Ser=0x7f040011;
-        public static final int Check_Num=0x7f040004;
-        public static final int Clientes=0x7f04001d;
-        public static final int EnBT=0x7f040021;
-        public static final int EncDev=0x7f04000c;
-        public static final int NB=0x7f04000a;
-        public static final int No_Dev=0x7f040020;
-        public static final int OK=0x7f040016;
-        public static final int Sel_BT=0x7f04001a;
-        public static final int Sel_BTs=0x7f04001c;
-        public static final int Sel_W=0x7f040019;
-        public static final int Sel_Ws=0x7f04001b;
-        public static final int Ser_Dat=0x7f040012;
-        public static final int Server_IP=0x7f040014;
-        public static final int Server_Port=0x7f040015;
-        public static final int Servidores=0x7f04001e;
-        public static final int SincDev=0x7f04000b;
-        public static final int Text_RX=0x7f040007;
-        public static final int Text_TX=0x7f040008;
-        public static final int Text_TXn=0x7f040009;
-        public static final int action_settings=0x7f040002;
-        public static final int app_Lab=0x7f040001;
-        public static final int app_name=0x7f040000;
-        public static final int case1=0x7f040026;
-        public static final int case2=0x7f040027;
-        public static final int case3=0x7f040028;
-        public static final int case4=0x7f040029;
-        public static final int case5=0x7f04002a;
-        public static final int case6=0x7f04002b;
-        public static final int case7=0x7f04002c;
-        public static final int case8=0x7f04002d;
-        public static final int case9=0x7f04002e;
-        public static final int device_list=0x7f04000d;
-        public static final int fin1=0x7f040024;
-        public static final int fin2=0x7f040025;
-        public static final int hello_world=0x7f040010;
-        public static final int main=0x7f040017;
-        public static final int set_server=0x7f040013;
-        public static final int title_activity_r34rt23=0x7f04002f;
-        public static final int title_activity_rbdojqw=0x7f040030;
+        public static final int BRX=0x7f05000e;
+        public static final int BSend=0x7f050005;
+        public static final int BSendn=0x7f050006;
+        public static final int BTX=0x7f05000f;
+        public static final int BT_Device=0x7f050022;
+        public static final int Button_Conect=0x7f050003;
+        public static final int Button_Des=0x7f05001f;
+        public static final int Button_Sel=0x7f050018;
+        public static final int Cancel_Conn=0x7f050023;
+        public static final int Chan_Ser=0x7f050011;
+        public static final int Check_Num=0x7f050004;
+        public static final int Clientes=0x7f05001d;
+        public static final int EnBT=0x7f050021;
+        public static final int EncDev=0x7f05000c;
+        public static final int NB=0x7f05000a;
+        public static final int No_Dev=0x7f050020;
+        public static final int OK=0x7f050016;
+        public static final int Sel_BT=0x7f05001a;
+        public static final int Sel_BTs=0x7f05001c;
+        public static final int Sel_W=0x7f050019;
+        public static final int Sel_Ws=0x7f05001b;
+        public static final int Ser_Dat=0x7f050012;
+        public static final int Server_IP=0x7f050014;
+        public static final int Server_Port=0x7f050015;
+        public static final int Servidores=0x7f05001e;
+        public static final int SincDev=0x7f05000b;
+        public static final int Text_RX=0x7f050007;
+        public static final int Text_TX=0x7f050008;
+        public static final int Text_TXn=0x7f050009;
+        public static final int action_settings=0x7f050002;
+        public static final int app_Lab=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int case1=0x7f050026;
+        public static final int case2=0x7f050027;
+        public static final int case3=0x7f050028;
+        public static final int case4=0x7f050029;
+        public static final int case5=0x7f05002a;
+        public static final int case6=0x7f05002b;
+        public static final int case7=0x7f05002c;
+        public static final int case8=0x7f05002d;
+        public static final int case9=0x7f05002e;
+        public static final int device_list=0x7f05000d;
+        public static final int fin1=0x7f050024;
+        public static final int fin2=0x7f050025;
+        public static final int hello_world=0x7f050010;
+        public static final int main=0x7f050017;
+        public static final int set_server=0x7f050013;
+        public static final int title_activity_r34rt23=0x7f05002f;
+        public static final int title_activity_rbdojqw=0x7f050030;
     }
     public static final class style {
         /** 
@@ -140,38 +140,16 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
-
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
- Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f070001;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
          */
-        public static final int Dialog=0x7f050002;
+        public static final int Dialog=0x7f070002;
     }
 }
